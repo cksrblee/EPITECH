d532c1fa44a432a3bef631ec9d90fcc9adbcc700
Lee Changyu <71711381+cksrblee@users.noreply.github.com>
2024-01-04 00:34:13 +0900

    Implement Scenario.json

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -8,10 +9,25 @@ public class GameManager : MonoBehaviour
     public static float moveScaleVariable = 1.0f;
 
     public UIController controller;
+    Scenarios scenarios;
 
     private void Awake()
     {
         controller = gameObject.GetComponent<UIController>();
+
+        //load json String Assets/Data/scenario.json
+        var jsonString = Resources.Load("scenario").ToString();
+        print(jsonString);
+
+        scenarios = JsonConvert.DeserializeObject<Scenarios>(jsonString);
+
+        foreach (Scenario scenario in scenarios.scenarios)
+        {
+            Debug.Log(scenario.king_id);
+            print(scenario.king_name);
+            // Further processing here
+        }
+
     }
 
     public UIController GetUIController()
