4c06234013d84e554a53f4af9d9833ac9ba1c4d8
Lee Changyu <71711381+cksrblee@users.noreply.github.com>
2024-01-03 23:16:28 +0900

    Complete Basic UI WireFrame

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static float moveScaleVariable = 1.0f;
 
+    public UIController controller;
 
+    private void Awake()
+    {
+        controller = gameObject.GetComponent<UIController>();
+    }
+
+    public UIController GetUIController()
+    {
+        return this.controller;
+    }
 }
